320e761d4f992a7d45b1a17036325f7ceb9509e9
Andre Alves <andrealvesaraujo.1995@gmail.com>
2025-04-17 17:00:55 -0300

    feat: :sparkles: create exit controller script that handle win condition

---
+++ Assets/AndreAssets/Scripts/ExitController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private int requiredBlue = 5;
+    [SerializeField] private int requiredRed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            Debug.Log($"Recurso blue: {player.recursoBlue} e red: {player.recursoRed}");
+
+            if(player.recursoBlue >= requiredBlue && player.recursoRed >= requiredRed){
+
+                Debug.Log("Ganhou o jogo");
+                return;
+            }
+            
+            Debug.Log($"Ainda não ganhou o jogo. Precisa de {requiredBlue} blue e {requiredRed} red.");
+        }
+        else
+        {
+            Debug.LogWarning("PlayerController component not found on the colliding object.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
