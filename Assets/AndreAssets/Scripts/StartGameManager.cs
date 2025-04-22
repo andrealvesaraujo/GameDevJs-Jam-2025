fa1c2e619e7f9f3e033bb1c670288157462526a5
Andre Alves <andrealvesaraujo.1995@gmail.com>
2025-04-22 19:31:21 -0300

    feat: :sparkles: create start menu for game in MainScene

---
+++ Assets/AndreAssets/Scripts/StartGameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("AndreScene");
+    }
+}
